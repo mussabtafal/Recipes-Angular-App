3771d6540bd7705e7035ed0ee40d45f2e8e7002e
mussab_tafal <mussab@procure.ps>
2023-12-04 13:20:08 +0200

    Recipe-project files.

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,42 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+    constructor(private slService: ShoppingListService) {};
+    private recipes: Recipe[] = [
+        new Recipe(
+        'Tasry Schnitzel', 
+        'A super tatsty Schniztzel - just Awesome !', 
+        'https://therecipecritic.com/wp-content/uploads/2020/10/pork-schnitzel-recipe-3-1063x1536.jpg',
+        [
+            new Ingredient('Meat', 1,),
+            new Ingredient('French Fries', 20,)
+        ]),
+        new Recipe(
+        'Shrimp Salad', 
+        'A vey healthy Shrimo Salad - Amazing !', 
+        'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+        [
+            new Ingredient('Shrimp', 10),
+            new Ingredient('Lettuce', 3),
+            new Ingredient('Tomatoes', 1)
+        ])
+    ];
+
+    getRecipeById(id: number) {
+        return this.recipes[id];
+    }
+    
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
+    }
+}
---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,22 @@
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrl: './shopping-edit.component.css'
+})
+export class ShoppingEditComponent {
+  @ViewChild('nameInput', {static: true}) nameInputRef: ElementRef;
+  @ViewChild('amountInput', {static: true}) amountInputRef: ElementRef;
+
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIng = new Ingredient(ingName, ingAmount)
+    this.shoppingListService.addIngredient(newIng);
+  }
+}
